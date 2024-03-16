c789907e35a74db7bc8e6ec35d687eb1d0b145e6
liabklein <liabklein@gmail.com>
2024-03-16 10:09:12 -0400

    add google analytics

--- weather-jacket-recommendation/client/src/App.js
+++ weather-jacket-recommendation/client/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';import './App.css';
 import JacketRecommendation from './JacketRecommendation';
 import Header from './Header';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-7LB6NCP94J";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
       <Header />
