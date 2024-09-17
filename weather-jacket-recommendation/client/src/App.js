7cce5b50cdeed329b346800fdc76a99fa039c318
liabklein <liabklein@gmail.com>
2024-09-17 08:59:28 -0400

    Update App.js

--- weather-jacket-recommendation/client/src/App.js
+++ weather-jacket-recommendation/client/src/App.js
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react';import './App.css';
+import React, { useEffect } from 'react';
+import './App.css';
 import JacketRecommendation from './JacketRecommendation';
 import Header from './Header';
-import ReactGA from 'react-ga';
 
 const TRACKING_ID = "G-7LB6NCP94J";
 
 function App() {
   useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    // Initialize GA4
+    const gtagScript = document.createElement('script');
+    gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`;
+    gtagScript.async = true;
+    document.head.appendChild(gtagScript);
+
+    gtagScript.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { window.dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', TRACKING_ID, {
+        page_path: window.location.pathname + window.location.search,
+      });
+    };
   }, []);
 
   return (
@@ -22,4 +34,3 @@ function App() {
 }
 
 export default App;
-
